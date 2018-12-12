3c5aeb2f551f9780c7ddb2fc4e82a0c220a2dfa2
israelemoibincom <israelemoi.bincom@gmail.com>
2018-12-12 13:28:01 +0100

    setting up CRUD mutations

--- src/graphqlModule.ts
+++ src/graphqlModule.ts
@@ -4,8 +4,8 @@ import { ApolloModule, Apollo} from 'apollo-angular';
 import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
-/* const simpleAPI = 'Your SimpleAPi Endpoint  from graphcool'; */
-const simpleAPI = 'https://api.graph.cool/simple/v1/cjpk9nbty8g8n01193cypf0f5';
+const simpleAPI = 'Your SimpleAPi Endpoint  from graphcool'; 
+
 
 @NgModule({
     exports: [
